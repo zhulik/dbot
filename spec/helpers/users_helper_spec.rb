137d8e4c626173be02993ae93a55c5fe1753da6b
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2017-12-31 00:20:35 +0300

    specs improved

---
+++ spec/helpers/users_helper_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+describe UsersHelper do
+  describe '#user_greeting' do
+    subject { helper.user_greeting(user) }
+
+    context 'with no data available' do
+      let(:user) { Telegram::Bot::Types::User.new }
+
+      it 'returns "my friend"' do
+        expect(subject).to eq('my friend')
+      end
+    end
+
+    context 'with username passed' do
+      let(:user) { Telegram::Bot::Types::User.new(username: 'user') }
+
+      it 'returns username' do
+        expect(subject).to eq('user')
+      end
+    end
+
+    context 'with only first_name and username passed' do
+      let(:user) { Telegram::Bot::Types::User.new(first_name: 'John', username: 'user') }
+
+      it 'returns username' do
+        expect(subject).to eq('user')
+      end
+    end
+
+    context 'with first_name, last name and username passed ' do
+      let(:user) { Telegram::Bot::Types::User.new(first_name: 'John', last_name: 'Smith', username: 'user') }
+
+      it 'returns name' do
+        expect(subject).to eq('John Smith')
+      end
+    end
+  end
+end

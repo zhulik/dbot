9f0f63e62633c2a58ed86c671e6880195d0d5553
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2018-01-04 05:42:03 +0300

    fixtures path fixed

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,7 +17,7 @@ require 'vcr'
 require 'sidekiq/testing'
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
 end
 

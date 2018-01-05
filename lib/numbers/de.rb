dbd8af504acddad2027e5b6750a65723fd995634
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2018-01-05 21:51:41 +0300

    German numbers improved

--- lib/numbers/de.rb
+++ lib/numbers/de.rb
@@ -1,90 +1,65 @@
 # frozen_string_literal: true
 
 class Numbers::De
-  DIGITS = {
-    '0' => 'null',
-    '1' => 'ein',
-    '2' => 'zwei',
-    '3' => 'drei',
-    '4' => 'vier',
-    '5' => 'fünf',
-    '6' => 'sechs',
-    '7' => 'sieben',
-    '8' => 'acht',
-    '9' => 'neun',
-    '10' => 'zehn',
-    '11' => 'elf',
-    '12' => 'zwölf',
-    '13' => 'dreizehn',
-    '14' => 'vierzehn',
-    '15' => 'fünfzehn',
-    '16' => 'sechzehn',
-    '17' => 'siebzehn',
-    '18' => 'achtzehn',
-    '19' => 'neunzehn',
-    '20' => 'zwanzig',
-    '30' => 'dreißig',
-    '40' =>	'vierzig',
-    '50' =>	'fünfzig',
-    '60' =>	'sechzig',
-    '70' =>	'siebzig',
-    '80' =>	'achtzig',
-    '90' =>	'neunzig',
-    '100' => 'hundert',
-    '1000' =>	'tausend'
-  }.freeze
+  DIGITS = YAML.load_file('lib/numbers/de.yml')['de']
 
   class << self
     delegate :words, to: :new
   end
 
   def words(number)
-    raise ArgumentError if number > 999_999
-    result = if DIGITS[number.to_s].present?
-               DIGITS[number.to_s]
-             else
-               thousands, number = number.to_s.split('').reverse.each_slice(3).to_a.map do |part|
-                 part.join.reverse.to_i
-               end.reverse
+    raise ArgumentError if number > 999_999_999_999 || number.negative?
+    return postprocess(DIGITS[number]) if DIGITS[number].present?
 
-               r = under_thousand(thousands)
-               r = r + DIGITS['1000'] + under_thousand(number) if number.present?
-               r
-             end
+    number = number.to_s.rjust(12, '0')
 
-    result += 's' if result.ends_with?('ein')
-    result
+    billions, millions, thousands, number = number.to_s.reverse.scan(/.{1,3}/).map do |part|
+      part.reverse.to_i
+    end.reverse
+
+    postprocess(process(billions, millions, thousands, number))
   end
 
   private
 
-  # rubocop:disable Metrics/MethodLength
-  def under_thousand(number)
-    digits = number.to_s.split('').reverse.map(&:to_i)
-    multiplier = 100
-    enum = digits.to_enum
-    index = -1
-    result = ''
-    loop do
-      digit = enum.next
-      index += 1
-      if index.zero?
-        result = under_hundred(digits)
-        enum.next
-        next
-      end
-      result = DIGITS[digit.to_s] + DIGITS[multiplier.to_s] + result
-      multiplier *= 10
+  def process(billions, millions, thousands, number)
+    result = under_thousand(number)
+    result = "#{under_thousand(thousands)}#{DIGITS[1000]}#{result}" unless thousands.zero?
+    unless millions.zero?
+      n = under_thousand(millions)
+      result = "#{n == 'ein' ? 'eine' : n} #{decline(millions, DIGITS[1_000_000])} #{result}"
+    end
+    unless billions.zero?
+      n = under_thousand(billions)
+      result = "#{n == 'ein' ? 'eine' : n} #{decline(billions, DIGITS[1_000_000_000])} #{result}"
     end
     result
   end
-  # rubocop:enable Metrics/MethodLength
+
+  def postprocess(result)
+    result += 's' if result.ends_with?('ein')
+    result = 'ein' + result if %w(hundert tausend).include?(result)
+    result = 'eine ' + result if %w(Million Milliarde).include?(result)
+    result.strip
+  end
+
+  def under_thousand(number)
+    digits = number.to_s.split('').reverse.map(&:to_i)
+    result = under_hundred(digits.first(2))
+    result = DIGITS[digits.last] + DIGITS[100] + result if digits.count > 2
+    result
+  end
 
   def under_hundred(digits)
-    return '' if digits[0].zero? && digits.one?
-    n = DIGITS["#{digits[1]}#{digits[0]}".to_i.to_s]
-    return '' if n == 'null'
+    return '' if digits.all?(&:zero?)
+    n = DIGITS["#{digits[1]}#{digits[0]}".to_i]
     return n if n.present?
-    DIGITS[digits[0].to_s] + 'und' + DIGITS[(digits[1] * 10).to_s]
+    DIGITS[digits[0]] + 'und' + DIGITS[(digits[1] * 10)]
+  end
+
+  def decline(count, number)
+    return number if count == 1
+    return number + 'n' if number.ends_with?('e')
+    number + 'en'
   end
 end
--- spec/lib/numbers/de_spec.rb
+++ spec/lib/numbers/de_spec.rb
@@ -2,36 +2,58 @@
 
 describe Numbers::De do
   describe '.words' do
-    it { expect(described_class.words(0)).to eq('null') }
-    it { expect(described_class.words(1)).to eq('eins') }
-    it { expect(described_class.words(4)).to eq('vier') }
-    it { expect(described_class.words(11)).to eq('elf') }
-    it { expect(described_class.words(12)).to eq('zwölf') }
-    it { expect(described_class.words(17)).to eq('siebzehn') }
-    it { expect(described_class.words(30)).to eq('dreißig') }
-    it { expect(described_class.words(41)).to eq('einundvierzig') }
-    it { expect(described_class.words(99)).to eq('neunundneunzig') }
-    it { expect(described_class.words(101)).to eq('einhunderteins') }
-    it { expect(described_class.words(111)).to eq('einhundertelf') }
-    it { expect(described_class.words(112)).to eq('einhundertzwölf') }
-    it { expect(described_class.words(117)).to eq('einhundertsiebzehn') }
-    it { expect(described_class.words(133)).to eq('einhundertdreiunddreißig') }
-    it { expect(described_class.words(300)).to eq('dreihundert') }
-    it { expect(described_class.words(843)).to eq('achthundertdreiundvierzig') }
-    it { expect(described_class.words(801)).to eq('achthunderteins') }
-    it { expect(described_class.words(810)).to eq('achthundertzehn') }
-    it { expect(described_class.words(999)).to eq('neunhundertneunundneunzig') }
-    it { expect(described_class.words(1000)).to eq('tausend') }
-    it { expect(described_class.words(2010)).to eq('zweitausendzehn') }
-    it { expect(described_class.words(2100)).to eq('zweitausendeinhundert') }
-    it { expect(described_class.words(2300)).to eq('zweitausenddreihundert') }
-    it { expect(described_class.words(2213)).to eq('zweitausendzweihundertdreizehn') }
-    it { expect(described_class.words(2254)).to eq('zweitausendzweihundertvierundfünfzig') } # my eyes!
-    it { expect(described_class.words(10_000)).to eq('zehntausend') }
-    it { expect(described_class.words(10_100)).to eq('zehntausendeinhundert') }
-    it { expect(described_class.words(12_100)).to eq('zwölftausendeinhundert') }
-    it { expect(described_class.words(20_100)).to eq('zwanzigtausendeinhundert') }
-    it { expect(described_class.words(20_200)).to eq('zwanzigtausendzweihundert') }
-    it { expect(described_class.words(543_481)).to eq('fünfhundertdreiundvierzigtausendvierhunderteinundachtzig') } # my fucking eyes!!!
+    {
+      0 => 'null',
+      1 => 'eins',
+      4 => 'vier',
+      11 => 'elf',
+      12 => 'zwölf',
+      17 => 'siebzehn',
+      30 => 'dreißig',
+      41 => 'einundvierzig',
+      99 => 'neunundneunzig',
+      100 => 'einhundert',
+      101 => 'einhunderteins',
+      111 => 'einhundertelf',
+      112 => 'einhundertzwölf',
+      117 => 'einhundertsiebzehn',
+      133 => 'einhundertdreiunddreißig',
+      300 => 'dreihundert',
+      843 => 'achthundertdreiundvierzig',
+      801 => 'achthunderteins',
+      810 => 'achthundertzehn',
+      999 => 'neunhundertneunundneunzig',
+      1000 => 'eintausend',
+      2010 => 'zweitausendzehn',
+      2100 => 'zweitausendeinhundert',
+      2300 => 'zweitausenddreihundert',
+      2213 => 'zweitausendzweihundertdreizehn',
+      2254 => 'zweitausendzweihundertvierundfünfzig', # my eyes!
+      10_000 => 'zehntausend',
+      10_100 => 'zehntausendeinhundert',
+      12_100 => 'zwölftausendeinhundert',
+      20_100 => 'zwanzigtausendeinhundert',
+      20_200 => 'zwanzigtausendzweihundert',
+      503_000 => 'fünfhundertdreitausend',
+      503_100 => 'fünfhundertdreitausendeinhundert',
+      543_481 => 'fünfhundertdreiundvierzigtausendvierhunderteinundachtzig', # my fucking eyes!,
+      727_727 => 'siebenhundertsiebenundzwanzigtausendsiebenhundertsiebenundzwanzig', # fuck!
+
+      1_000_000 => 'eine Million',
+      2_000_000 => 'zwei Millionen',
+      1_000_001 => 'eine Million eins',
+      2_000_001 => 'zwei Millionen eins',
+      727_727_727 => 'siebenhundertsiebenundzwanzig Millionen siebenhundertsiebenundzwanzigtausendsiebenhundertsiebenundzwanzig', # AHAHAHAHA
+      1_000_000_000 => 'eine Milliarde',
+      2_000_000_000 => 'zwei Milliarden',
+      2_001_000_000 => 'zwei Milliarden eine Million',
+      2_002_000_000 => 'zwei Milliarden zwei Millionen',
+      213_431_983_111 => 'zweihundertdreizehn Milliarden vierhunderteinunddreißig Millionen neunhundertdreiundachtzigtausendeinhundertelf'
+
+    }.each do |number, words|
+      it "for #{number} it returns #{words}" do
+        expect(described_class.words(number)).to eq(words)
+      end
+    end
   end
 end

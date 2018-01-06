440ac058cfb2893515705891d432d11b7d1d7ca9
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2018-01-06 23:39:49 +0300

    pronounce command improved

--- app/controllers/concerns/pronounce_command.rb
+++ app/controllers/concerns/pronounce_command.rb
@@ -3,8 +3,25 @@
 module PronounceCommand
   extend ActiveSupport::Concern
 
+  included do
+    context_handler :pronounce_send_sentence do |*ws|
+      pronounce_direct(ws.join(' '))
+    end
+  end
+
   def pronounce(*ws)
-    phrase = ws.join(' ')
+    return pronounce_full if ws.empty?
+    pronounce_direct(ws.join(' '))
+  end
+
+  private
+
+  def pronounce_full
+    save_context :pronounce_send_sentence
+    respond_with :message, text: t('common.send_sentence')
+  end
+
+  def pronounce_direct(phrase)
     TTS::CachedTTS.new(phrase, current_user.language).get do |payload|
       respond_with :voice, voice: payload
     end

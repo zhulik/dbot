d8dc15a5ba98de4cd561f54c2261926531a9d364
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2018-01-06 20:28:25 +0300

    fix

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require_relative 'boot'
 require 'rails/all'
 require 'telegram/bot'
 require 'telegram/bot/types'
+require 'named_string_io'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.

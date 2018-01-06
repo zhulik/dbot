bc62656f55a6dd660f76b28faa807b64fbe78d99
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2018-01-06 20:47:22 +0300

    storage removed

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module Dbot
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.active_storage.service = Rails.env
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers

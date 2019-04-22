f3e4f0202b752fae2f6c6a5d65949e6a761d3f52
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2019-04-22 16:48:07 +0200

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Sidekiq::Web.use Rack::Auth::Basic do |username, password|
   ActiveSupport::SecurityUtils.secure_compare(Digest::SHA256.hexdigest("#{username}:#{password}:#{salt}"), AUTH_HASH)
 end
 
+Telegram.bots_config = Telegram.bots_config.presence || { default: { token: '123' } }
+
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/admin/sidekiq'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'

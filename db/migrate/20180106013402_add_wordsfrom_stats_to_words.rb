93674e511d0ed74b0db40f585037016d2be8152d
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2019-04-22 16:51:51 +0200

    Fix rubocop

--- db/migrate/20180106013402_add_wordsfrom_stats_to_words.rb
+++ db/migrate/20180106013402_add_wordsfrom_stats_to_words.rb
@@ -2,7 +2,7 @@
 
 class AddWordsfromStatsToWords < ActiveRecord::Migration[5.1]
   def change
-    add_column :words, :wordsfrom_success, :integer, null: false, default: 0 # rubocop:disable Rails/BulkChangeTable
+    add_column :words, :wordsfrom_success, :integer, null: false, default: 0
     add_column :words, :wordsfrom_fail, :integer, null: false, default: 0
   end
 end
--- db/migrate/20180122000228_add_message_id_to_practice_stat.rb
+++ db/migrate/20180122000228_add_message_id_to_practice_stat.rb
@@ -3,7 +3,7 @@
 class AddMessageIdToPracticeStat < ActiveRecord::Migration[5.1]
   def change
     # rubocop:disable Rails/NotNullColumn
-    add_column :practice_stats, :message_id, :bigint, null: false # rubocop:disable Rails/BulkChangeTable
+    add_column :practice_stats, :message_id, :bigint, null: false
     add_column :practice_stats, :chat_id, :bigint, null: false
     add_index :practice_stats, %i[message_id chat_id], unique: true
     # rubocop:enable Rails/NotNullColumn

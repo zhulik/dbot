17dabce3c8de791093369640ce3b5178498e0fcf
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2019-05-01 15:34:18 +0200

    Fix app config

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(*Rails.groups)
 module Dbot
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
     config.active_job.queue_adapter = :sidekiq
 
     # Settings in config/environments/* take precedence over those specified here.

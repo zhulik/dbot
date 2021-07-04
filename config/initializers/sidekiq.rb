13ec933941532eedb521abfd49e2c8f6af7c1d94
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-07-05 01:18:44 +0200

    Configure sidekiq redis url using env

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,3 +3,11 @@
 SCHEDULE_FILE = 'config/schedule.yml'
 
 Sidekiq::Cron::Job.load_from_hash(YAML.load_file(SCHEDULE_FILE)) if File.exist?(SCHEDULE_FILE) && Sidekiq.server?
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch('SIDEKIQ_REDIS_URL'), namespace: 'dbot' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch('SIDEKIQ_REDIS_URL'), namespace: 'dbot' }
+end
